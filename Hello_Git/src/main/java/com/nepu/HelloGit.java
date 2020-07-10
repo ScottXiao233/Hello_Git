333f83294dd8fe39364d705054629d4255d884c5
Scott Xiao <285584108@qq.com>
2020-07-10 17:06:45 +0800

    this is update version 1.0 this is update version 1.0

--- Hello_Git/src/main/java/com/nepu/HelloGit.java
+++ Hello_Git/src/main/java/com/nepu/HelloGit.java
@@ -4,5 +4,8 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("update version 1");
+        System.out.println("update version 2");
+        System.out.println("update version 3");
+        System.out.println("update version 4");
     }
 }
