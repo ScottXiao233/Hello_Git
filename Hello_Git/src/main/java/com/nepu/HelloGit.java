0253e58f93f9ca99c402a869e5cf6d2182f788fc
Scott Xiao <285584108@qq.com>
2020-07-10 17:06:26 +0800

    this is update version 1.0

---
+++ Hello_Git/src/main/java/com/nepu/HelloGit.java
@@ -0,0 +1,8 @@
+package com.nepu;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+        System.out.println("update version 1");
+    }
+}
